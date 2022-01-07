cf47958a346c6edd126c0c89358ec0b99a65cb8c
Grace Fallon <graceleefallon@gmail.com>
2022-01-06 22:21:42 -0600

    use modulo and temp var to determine if palendrome

---
+++ 0x07-palindrome_integer/0-is_palindrome.c
@@ -0,0 +1,39 @@
+#include "palindrome.h"
+
+
+/**
+ * is_palindrome - checks if an unsigned long int is a palindrome,
+ * notably, tasked with not allocating addl memory, otherwise
+ * a recursive version would be more efficient in terms of time complexity
+ * @n: ulong to evaluate
+ * Return: 1 if palindrome, else 0
+**/
+int is_palindrome(unsigned long n)
+{
+    /* int var for last digit, building a reverse version, */
+    /* and a manipulable copy of input */
+    int lastDig = 0;
+    unsigned long int tempN = n, reverse = 0;
+    
+    /* work with positive numbers, logic the same */
+    /* probably not necessary for ulong */
+    if (n < 0)
+    {
+        n = n * -1;
+    }
+
+    tempN = n;
+
+    /* while depleting value is greater than 0 */
+    while (tempN)
+    {
+        /* isolate last digit */
+        lastDig = tempN % 10;
+        /* append last digit to build reverse of input */
+        reverse = (reverse * 10) + lastDig;
+        /* deplete temporary var */
+        tempN = tempN / 10;
+    }
+    
+    return (reverse == n);
+}
---
+++ 0x07-palindrome_integer/README.md
@@ -0,0 +1,4 @@
+# Holberton School AR/VR Specialization Interview Questions 
+Task: Without allocating additional memory, determine whether an input unsigned long integer is a palindrome
+
+# Created and compiled by Grace Fallon
